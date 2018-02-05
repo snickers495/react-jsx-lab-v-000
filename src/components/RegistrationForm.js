fcffd8003b1d60e7089a8210937e8678c36b5c67
Lisa Lee <snickers495@gmail.com>
2018-02-05 15:37:02 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,3 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-export default class RegistrationForm extends React.Component 
+export default class RegistrationForm extends React.Component {
+  render
+}
