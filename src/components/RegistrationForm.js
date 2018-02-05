99f1ba399b47f96f5a0519e03b266f2ffb00b30c
Lisa Lee <snickers495@gmail.com>
2018-02-05 15:37:45 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -5,7 +5,7 @@ export default class RegistrationForm extends React.Component {
     return(
       <form>
       <input type='text'/>
-      <input type=
+      <input type='password'/>
       </form>
     )
   }
